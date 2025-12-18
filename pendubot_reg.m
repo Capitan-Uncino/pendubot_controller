d9cfc801ae7148bc0c20d8546e891ce647187d24
Lorenzo <lorenzo.dispensa@student.unisi.it>
2025-12-19 00:53:29 +0100

    spacing between methods

--- pendubot_reg.m
+++ pendubot_reg.m
@@ -1,4 +1,5 @@
 function u=pendubot_reg(alpha1r,x)
+% % C1================================
 % %function u=pendubot_reg(alpha21,x)
 % %
 % %INPUTS:
@@ -14,7 +15,6 @@ function u=pendubot_reg(alpha1r,x)
 % % u       : control signal (motor torque) to be applied [Nm]
 % 
 % 
-% 
 % %u=3*((alpha1r-pi)-x(1))-0.3*x(3);
 % x_e = [alpha1r; pi; 0; 0];
 % % x_e = [alpha1r-pi; 0; 0; 0];
@@ -28,6 +28,8 @@ function u=pendubot_reg(alpha1r,x)
 
 %========================================================
 % % SWING UP
+
+% % 1======================Bad
 % % xi = x'+ [pi; pi; 0; 0];
 % xi = [x(1)+pi; x(2)+pi; x(3); x(4)];
 % 
@@ -122,6 +124,7 @@ Vmda = p3*sin(alpha2-alpha1) * [-dalpha2*dalpha1 + dalpha1^2 - dalpha2^2; dalpha
 % Gravity terms
 G = [p4*g*sin(alpha1); p5*g*sin(alpha2)];
 
+% 2=====================================
 % -------------------------------------------------------
 % Partial Feedback Linearization
 % Reduced dynamics
@@ -129,113 +132,169 @@ m11_hat   = m11 - (m12*m21)/m22;
 Vmda1_hat = Vmda(1) - (m12/m22)*Vmda(2);
 G1_hat    = G(1)    - (m12/m22)*G(2);
 
-% % alpha_e1 = alpha1r-xi(1);
-% % alpha_e2 = alpha1r-xi(2);
-% delta = 5;
-% lim_inf = alpha1r - delta/2;
-% lim_sup = alpha1r + delta/2;
-% kp = 25;     % <-- sostituisci con i tuoi
-% kd = 15;
-% K = [-0.2165, -16.2534, -1.2892, -2.1864];
-% % K = [-2, -15, -3, -5];
-% 
-% 
-% if xi(1) > lim_inf && xi(1) < alpha1r
-%     V1 = -kp*(xi(1) - delta/2) - kd*xi(3);
-%     if xi(2) > lim_inf/2 && xi(2) < alpha1r
-%         x_e = [alpha1r; pi; 0; 0];
-%         u_e = -1.7205;
-%         u = u_e - K*(xi-x_e);
-%         u_max = 8;   % Volt (esempio)
-%         u = max(min(u, u_max), -u_max);
-%         return
-%     elseif xi(2) > alpha1r && xi(2) < lim_sup
-%         x_e = [alpha1r; pi; 0; 0];
-%         u_e = -1.7205;
-%         u = u_e - K*(xi-x_e);
-%         u_max = 8;   % Volt (esempio)
-%         u = max(min(u, u_max), -u_max);
-%         return
-%     end
-% 
-% elseif xi(1) > alpha1r && xi(1) < lim_sup
-%     V1 = -kp*(xi(1) + delta/2) - kd*xi(3);
-%     if xi(2) > lim_inf/2 && xi(2) < alpha1r
-%         x_e = [alpha1r; pi; 0; 0];
-%         u_e = -1.7205;
-%         u = u_e - K*(xi-x_e);
-%         u_max = 8;   % Volt (esempio)
-%         u = max(min(u, u_max), -u_max);
-%         return
-%     elseif xi(2) > alpha1r && xi(2) < lim_sup
-%     x_e = [alpha1r; pi; 0; 0];
-%     u_e = -1.7205;
-%     u = (u_e - K*(xi-x_e));
-%     u_max = 8;   % Volt (esempio)
-%     u = max(min(u, u_max), -u_max);
-%     return
-%     end
-% else 
-%     % Virtual control (PD on first joint)
-%     V1 = -kp*(xi(1) - alpha1r) - kd*xi(3);
-% end
-% 
-% % if x(2) > lim_inf && x(2) < alpha1r
-% %         x_e = [alpha1r; pi; 0; 0];
-% %         u_e = -1.7205;
-% %         K = [-0.2165, -16.2534, -1.2892, -2.1864];
-% %         u = u_e - K*(xi-x_e);
-% %         u_max = 8;   % Volt (esempio)
-% %         u = max(min(u, u_max), -u_max);
-% %         return
-% %     end
-% 
-% % Torque
-% tau = m11_hat*V1 + Vmda1_hat + G1_hat;
-% 
-% % U = [p4*g*sin(alpha1r);
-% %      p5*g*sin(alpha1r)];
-% % tau_ref = U;
-% 
-% % Motor input
-% u = tau / k;
-% 
-% u_max = 8;   % Volt (esempio)
-% u = max(min(u, u_max), -u_max);
+% alpha_e1 = alpha1r-xi(1);
+% alpha_e2 = alpha1r-xi(2);
+delta = 5;
+lim_inf = alpha1r - delta/2;
+lim_sup = alpha1r + delta/2;
+kp = 25;     % <-- sostituisci con i tuoi
+kd = 15;
+K = [-0.2165, -16.2534, -1.2892, -2.1864];
+% K = [-2, -15, -3, -5];
 
-% ---------- LQR REGION ----------
-alpha_tol  = 0.15;     % rad
-dalpha_tol = 0.4;
 
-use_LQR = abs(alpha1-alpha1r) < alpha_tol && ...
-          abs(alpha2-pi)      < alpha_tol && ...
-          abs(dalpha1)        < dalpha_tol && ...
-          abs(dalpha2)        < dalpha_tol;
+if xi(1) > lim_inf && xi(1) < alpha1r
+    V1 = -kp*(xi(1) - delta/2) - kd*xi(3);
+    if xi(2) > lim_inf/2 && xi(2) < alpha1r
+        x_e = [alpha1r; pi; 0; 0];
+        u_e = -1.7205;
+        u = u_e - K*(xi-x_e);
+        u_max = 8;   % Volt (esempio)
+        u = max(min(u, u_max), -u_max);
+        return
+    elseif xi(2) > alpha1r && xi(2) < lim_sup
+        x_e = [alpha1r; pi; 0; 0];
+        u_e = -1.7205;
+        u = u_e - K*(xi-x_e);
+        u_max = 8;   % Volt (esempio)
+        u = max(min(u, u_max), -u_max);
+        return
+    end
 
-if use_LQR
-    % Equilibrium
+elseif xi(1) > alpha1r && xi(1) < lim_sup
+    V1 = -kp*(xi(1) + delta/2) - kd*xi(3);
+    if xi(2) > lim_inf/2 && xi(2) < alpha1r
+        x_e = [alpha1r; pi; 0; 0];
+        u_e = -1.7205;
+        u = u_e - K*(xi-x_e);
+        u_max = 8;   % Volt (esempio)
+        u = max(min(u, u_max), -u_max);
+        return
+    elseif xi(2) > alpha1r && xi(2) < lim_sup
     x_e = [alpha1r; pi; 0; 0];
     u_e = -1.7205;
-
-    % LQR gain
-    K = [-0.2165, -16.2534, -1.2892, -2.1864];
-
-    tau = u_e - K*(xi - x_e);
-
-else
-    % ---------- PFL + PD ----------
-    kp = 25;
-    kd = 15;
-
-    % Virtual control
-    V1 = -kp*(alpha1 - alpha1r) - kd*dalpha1;
-
-    % Torque
-    tau = m11_hat*V1 + Vmda1_hat + G1_hat;
+    u = (u_e - K*(xi-x_e));
+    u_max = 8;   % Volt (esempio)
+    u = max(min(u, u_max), -u_max);
+    return
+    end
+else 
+    % Virtual control (PD on first joint)
+    V1 = -kp*(xi(1) - alpha1r) - kd*xi(3);
 end
 
-%% ===== MOTOR INPUT =====
+% if x(2) > lim_inf && x(2) < alpha1r
+%         x_e = [alpha1r; pi; 0; 0];
+%         u_e = -1.7205;
+%         K = [-0.2165, -16.2534, -1.2892, -2.1864];
+%         u = u_e - K*(xi-x_e);
+%         u_max = 8;   % Volt (esempio)
+%         u = max(min(u, u_max), -u_max);
+%         return
+%     end
+
+% Torque
+tau = m11_hat*V1 + Vmda1_hat + G1_hat;
+
+% U = [p4*g*sin(alpha1r);
+%      p5*g*sin(alpha1r)];
+% tau_ref = U;
+
+% Motor input
 u = tau / k;
+
 u = max(min(u, u_max), -u_max);
+
+% 3========================================
+% % ---------- LQR REGION ----------
+% alpha_tol  = 0.15;     % rad
+% dalpha_tol = 0.4;
+% 
+% use_LQR = abs(alpha1-alpha1r) < alpha_tol && ...
+%           abs(alpha2-pi)      < alpha_tol && ...
+%           abs(dalpha1)        < dalpha_tol && ...
+%           abs(dalpha2)        < dalpha_tol;
+% 
+% if use_LQR
+%     % Equilibrium
+%     x_e = [alpha1r; pi; 0; 0];
+%     u_e = -1.7205;
+% 
+%     % LQR gain
+%     K = [-0.2165, -16.2534, -1.2892, -2.1864];
+% 
+%     tau = u_e - K*(xi - x_e);
+% 
+% else
+%     % ---------- PFL + PD ----------
+%     kp = 25;
+%     kd = 15;
+% 
+%     % Virtual control
+%     V1 = -kp*(alpha1 - alpha1r) - kd*dalpha1;
+% 
+%     % Torque
+%     tau = m11_hat*V1 + Vmda1_hat + G1_hat;
+% end
+% 
+% %% ===== MOTOR INPUT =====
+% u = tau / k;
+% u = max(min(u, u_max), -u_max);
+% 
+% end
+
+% 4===========================
+% %% =================================================
+% %% ========== 1) SWING-UP ENERGETICO ================
+% %% =================================================
+% 
+% % Energia del secondo link (target = upright)
+% E  = 0.5*p2*dalpha2^2 + p5*g*(1 - cos(alpha2));
+% E0 = 2*p5*g;   % energia desiderata (pi rad)
+% 
+% kE = 3;        % gain energetico
+% 
+% u_swing = kE * (E - E0) * sign(dalpha2*cos(alpha2));
+% 
+% %% =================================================
+% %% ========== 2) PFL + PD ==========================
+% %% =================================================
+% 
+% kp = 25;
+% kd = 15;
+% 
+% V1 = -kp*(alpha1 - alpha1r) - kd*dalpha1;
+% tau_pfl = m11_hat*V1 + Vmda1_hat + G1_hat;
+% 
+% %% =================================================
+% %% ========== 3) LQR LOCALE ========================
+% %% =================================================
+% 
+% x_e = [alpha1r; pi; 0; 0];
+% u_e = -1.7205;
+% K   = [-0.2165, -16.2534, -1.2892, -2.1864];
+% 
+% tau_lqr = u_e - K*(xi - x_e);
+% 
+% %% =================================================
+% %% ========== BLENDING CONTINUO ====================
+% %% =================================================
+% 
+% % distanza dall’equilibrio
+% d = norm([alpha1-alpha1r;
+%           alpha2-pi;
+%           dalpha1;
+%           dalpha2]);
+% 
+% % pesi smooth
+% w_lqr   = exp(-4*d^2);
+% w_pfl   = (1-w_lqr) * exp(-0.5*d^2);
+% w_swing = 1 - w_lqr - w_pfl;
+% 
+% tau = w_swing*u_swing + w_pfl*tau_pfl + w_lqr*tau_lqr;
+% 
+% %% ===== MOTOR INPUT =====
+% u = tau / k;
+% u = max(min(u, u_max), -u_max);
 
 end
