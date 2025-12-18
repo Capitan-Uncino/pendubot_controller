6541ba9937010a50a1e165b7cd523cd6e31452ea
Lorenzo <lorenzo.dispensa@student.unisi.it>
2025-12-19 00:33:58 +0100

    updated swing up with 3 methods not working

--- SolveSyms.m
+++ SolveSyms.m
@@ -4,7 +4,7 @@ clc
 
 %pendubot's equations
 
-syms x1 x2 x3 x4 u   %alpha1, alpha2, dalpha1/dt, dalpha2/dt 
+syms x1 x2 x3 x4 v1 k1 k2  %alpha1, alpha2, dalpha1/dt, dalpha2/dt 
 
 x = [x1; x2; x3; x4];
 n = length(x);
@@ -20,47 +20,60 @@ g=9.81;
 
 k = 3.9621/8;%V (Nm)^â?»1
 
-%M(alpha)
-m11 = p1 + p2 + 2*p3*cos(x2-x1);  m12 = p2 + p3*cos(x2-x1);
-m21 = p2 + p3*cos(x2-x1);         m22 = p2;
-M=[m11 m12; m21 m22];
+% %M(alpha)
+% m11 = p1 + p2 + 2*p3*cos(x2-x1);  m12 = p2 + p3*cos(x2-x1);
+% m21 = p2 + p3*cos(x2-x1);         m22 = p2;
+% M=[m11 m12; m21 m22];
+% 
+% %Vm(alpha, dalpha/dt)
+% Vm = p3*sin(x2-x1)*[x3-x4 -x4; x3 0];
+% 
+% %G(alpha)
+% G = [p4*g*sin(x1); p5*g*sin(x2)];
+% 
+% %equations
+% fx = [x3; x4; M\(-Vm*[x3; x4]-G)];
+% gx = [0; 0; M\[k; 0]];
+% 
+% % Linearization for equilibrium point x_e and input u_e
+% x_e = [4/3*pi; pi; 0; 0]; 
+% 
+% eqns = [x3 == 0; x4 == 0; M\(-Vm*[x3; x4]-G) + M\[k; 0]*u == 0];
+% u_e = double(vpa(solve(subs(eqns, x, x_e ), u), 5));
+% 
+% A = double(vpa(subs(jacobian(fx + gx*u, x), [x; u], [x_e; u_e]), 3));
+% B = double(vpa(subs(jacobian(fx + gx*u, u), x, x_e), 3));
+% 
+% C = eye(n);
+% D = zeros(n,1);
 
-%Vm(alpha, dalpha/dt)
-Vm = p3*sin(x2-x1)*[x3-x4 -x4; x3 0];
+xs = [x1; x2; x3; x4];
+
+% New state variables
+z1 = x1-pi;
+eta1 = x2;
+z2 = x3;
+eta2 = x4;
+
+w = [z1; z2; eta1; eta2];
+
+% Equilibrium point
+x_e = [pi; pi; 0; 0];
+
+%M(alpha)
+m11 = p1 + p2 + 2*p3*cos(eta1-z1);  m12 = p2 + p3*cos(eta1-z1);
+m21 = p2 + p3*cos(eta1-z1);         m22 = p2;
+
+%Vm(alpha, dalpha/dt)*dalpha/dt
+Vmda = p3*sin(eta1-z1)*[-eta2*z2 + z2^2 - eta2^2 ; z2^2];
 
 %G(alpha)
-G = [p4*g*sin(x1); p5*g*sin(x2)];
+G = [p4*g*sin(z1); p5*g*sin(eta1)];
 
-%equations
-fx = [x3; x4; M\(-Vm*[x3; x4]-G)];
-gx = [0; 0; M\[k; 0]];
-% f34 = M\(-Vm*[x3; x4]-G);
-% g34 = M\[k; 0];
-% eqn = fx + gx*u == 0;
-% eqns = [x3 == 0; x4 == 0; f34 + g34*u == 0];
-% S = solve(eqns, [x1 x2 x3 x4 u])
+% Solve for equilibrium input
+eqns = [z2 == 0; z1 == 0; eta2 == 0; -1/m22 * (Vmda(2)+G(2)) - (m12/m22 * v1) == 0];
+v1_e = double(vpa(solve(subs(eqns, w, x_e ), v1), 5));
 
-% Linearization for equilibrium point x_e and input u_e
-x_e = [0; 0; 0; 0];
-% x_dot = fx + gx*u == 0;
-% eqn = fx + gx*u == 0;
-eqns = [x3 == 0; x4 == 0; M\(-Vm*[x3; x4]-G) + M\[k; 0]*u == 0];
-u_e = vpa(solve(subs(eqns, x, x_e ), u), 5);
-u_e = double(u_e);
-% u_e = 5;
+% [kp, kd] = solve(k1 >= 0, k2 >= 0, v1_e == k1*(pi-x1)-k2*x3, x_e);
 
-% A1 = jacobian(fx + gx*u, x)
-% B1 = jacobian(fx + gx*u, u)
-% A2 = vpa(subs(A, x, x_e),2)
-% % A = vpa(subs(A, [x; u], [x_ref; u_e]),2)
-% B2 = vpa(subs(B, x, x_ref),2)
-% A = vpa(subs(A, u, S),2)
-% B = vpa(subs(B, u, S),2)
-
-A = vpa(subs(jacobian(fx + gx*u, x), [x; u], [x_e; u_e]), 3);
-A = double(A);
-B = vpa(subs(jacobian(fx + gx*u, u), x, x_e), 3);
-B = double(B);
-
-C = eye(n);
-D = zeros(n,1);
+V1 = kp*(pi-x1)-kd*x3;
--- pendubot_reg.m
+++ pendubot_reg.m
@@ -27,67 +27,215 @@ function u=pendubot_reg(alpha1r,x)
 % % u = 0;
 
 %========================================================
-% SWING UP
-% xi = x'+ [pi; pi; 0; 0];
+% % SWING UP
+% % xi = x'+ [pi; pi; 0; 0];
+% xi = [x(1)+pi; x(2)+pi; x(3); x(4)];
+% 
+% p1=0.0148;%kg m^2
+% p2=0.0051;
+% p3=0.0046;
+% p4=0.1003;%kg m
+% p5=0.0303;
+% g=9.81;
+% 
+% k = 3.9621/8;%V (Nm)^â?»1
+% 
+% sym x1 x2 x3 x4 v1 k1 k2
+% 
+% % xs = [x1; x2; x3; x4];
+% 
+% % New state variables
+% z1 = x1-alpha1r;
+% eta1 = x2;
+% z2 = x3;
+% eta2 = x4;
+% 
+% w = [z1; z2; eta1; eta2];
+% 
+% % Equilibrium point
+% x_e = [alpha1r; pi; 0; 0];
+% 
+% %M(alpha)
+% m11 = p1 + p2 + 2*p3*cos(eta1-z1);  m12 = p2 + p3*cos(eta1-z1);
+% m21 = p2 + p3*cos(eta1-z1);         m22 = p2;
+% 
+% %Vm(alpha, dalpha/dt)*dalpha/dt
+% Vmda = p3*sin(eta1-z1)*[-eta2*z2 + z2^2 - eta2^2 ; z2^2];
+% 
+% %G(alpha)
+% G = [p4*g*sin(z1); p5*g*sin(eta1)];
+% 
+% % Solve for equilibrium input
+% eqns = [z2 == 0; z1 == 0; eta2 == 0; -1/m22 * (Vmda(2)+G(2)) - (m12/m22 * v1) == 0];
+% v1_e = double(vpa(solve(subs(eqns, w, x_e ), v1), 5));
+% 
+% [kp, kd] = solve(k1 > 0, k2 > 0, v1_e == k1*(alpha1r-x1)-k2*x3, x, x_e);
+% 
+% V1 = kp*(alpha1r-x1)-kd*x3;
+% 
+% % Partial Feedback Linearization
+% %M(alpha)
+% m11 = p1 + p2 + 2*p3*cos(xi(2)-xi(1));  m12 = p2 + p3*cos(xi(2)-xi(1));
+% m21 = p2 + p3*cos(xi(2)-xi(1));         m22 = p2;
+% 
+% %Vm(alpha, dalpha/dt)*dalpha/dt
+% Vmda = p3*sin(xi(2)-xi(1))*[-xi(4)*xi(3) + xi(3)^2 - xi(4)^2 ; xi(3)^2];
+% 
+% %G(alpha)
+% G = [p4*g*sin(xi(1)); p5*g*sin(xi(2))];
+% 
+% m11_hat = m11 - (m12*m21/m22);
+% Vmda1_hat = Vmda(1) - (m12*Vmda(2)/m22);
+% G1_hat = G(1) - (m12*G(2)/m22);
+% 
+% tau = m11_hat*V1 + Vmda1_hat + G1_hat;
+% u = tau/k;
+
+%========================================================
+% STATE RECONSTRUCTION
 xi = [x(1)+pi; x(2)+pi; x(3); x(4)];
+alpha1 = xi(1);
+alpha2 = xi(2);
+dalpha1 = xi(3);
+dalpha2 = xi(4);
 
-p1=0.0148;%kg m^2
-p2=0.0051;
-p3=0.0046;
-p4=0.1003;%kg m
-p5=0.0303;
-g=9.81;
+% Physical parameters
+p1 = 0.0148;   % kg m^2
+p2 = 0.0051;
+p3 = 0.0046;
+p4 = 0.1003;   % kg m
+p5 = 0.0303;
+g  = 9.81;
 
-k = 3.9621/8;%V (Nm)^â?»1
+k = 3.9621/8;  % motor constant (Nm/V)
+u_max   = 8;
 
-sym x1 x2 x3 x4 v1 k1 k2
+% Mass matrix
+m11 = p1 + p2 + 2*p3*cos(alpha2-alpha1);
+m12 = p2 + p3*cos(alpha2-alpha1);
+m21 = m12;
+m22 = p2;
 
-% xs = [x1; x2; x3; x4];
+% Coriolis / centrifugal terms
+Vmda = p3*sin(alpha2-alpha1) * [-dalpha2*dalpha1 + dalpha1^2 - dalpha2^2; dalpha1^2];
 
-% New state variables
-z1 = x1-alpha1r;
-eta1 = x2;
-z2 = x3;
-eta2 = x4;
+% Gravity terms
+G = [p4*g*sin(alpha1); p5*g*sin(alpha2)];
 
-w = [z1; z2; eta1; eta2];
-
-% Equilibrium point
-x_e = [alpha1r; pi; 0; 0];
-
-%M(alpha)
-m11 = p1 + p2 + 2*p3*cos(eta1-z1);  m12 = p2 + p3*cos(eta1-z1);
-m21 = p2 + p3*cos(eta1-z1);         m22 = p2;
-
-%Vm(alpha, dalpha/dt)*dalpha/dt
-Vmda = p3*sin(eta1-z1)*[-eta2*z2 + z2^2 - eta2^2 ; z2^2];
-
-%G(alpha)
-G = [p4*g*sin(z1); p5*g*sin(eta1)];
-
-% Solve for equilibrium input
-eqns = [z2 == 0; z1 == 0; eta2 == 0; -1/m22 * (Vmda(2)+G(2)) - (m12/m22 * v1) == 0];
-v1_e = double(vpa(solve(subs(eqns, w, x_e ), v1), 5));
-
-[kp, kd] = solve(k1 > 0, k2 > 0, v1_e == k1*(alpha1r-x1)-k2*x3, x, x_e);
-
-V1 = kp*(alpha1r-x1)-kd*x3;
-
+% -------------------------------------------------------
 % Partial Feedback Linearization
-%M(alpha)
-m11 = p1 + p2 + 2*p3*cos(xi(2)-xi(1));  m12 = p2 + p3*cos(xi(2)-xi(1));
-m21 = p2 + p3*cos(xi(2)-xi(1));         m22 = p2;
+% Reduced dynamics
+m11_hat   = m11 - (m12*m21)/m22;
+Vmda1_hat = Vmda(1) - (m12/m22)*Vmda(2);
+G1_hat    = G(1)    - (m12/m22)*G(2);
 
-%Vm(alpha, dalpha/dt)*dalpha/dt
-Vmda = p3*sin(xi(2)-xi(1))*[-xi(4)*xi(3) + xi(3)^2 - xi(4)^2 ; xi(3)^2];
+% % alpha_e1 = alpha1r-xi(1);
+% % alpha_e2 = alpha1r-xi(2);
+% delta = 5;
+% lim_inf = alpha1r - delta/2;
+% lim_sup = alpha1r + delta/2;
+% kp = 25;     % <-- sostituisci con i tuoi
+% kd = 15;
+% K = [-0.2165, -16.2534, -1.2892, -2.1864];
+% % K = [-2, -15, -3, -5];
+% 
+% 
+% if xi(1) > lim_inf && xi(1) < alpha1r
+%     V1 = -kp*(xi(1) - delta/2) - kd*xi(3);
+%     if xi(2) > lim_inf/2 && xi(2) < alpha1r
+%         x_e = [alpha1r; pi; 0; 0];
+%         u_e = -1.7205;
+%         u = u_e - K*(xi-x_e);
+%         u_max = 8;   % Volt (esempio)
+%         u = max(min(u, u_max), -u_max);
+%         return
+%     elseif xi(2) > alpha1r && xi(2) < lim_sup
+%         x_e = [alpha1r; pi; 0; 0];
+%         u_e = -1.7205;
+%         u = u_e - K*(xi-x_e);
+%         u_max = 8;   % Volt (esempio)
+%         u = max(min(u, u_max), -u_max);
+%         return
+%     end
+% 
+% elseif xi(1) > alpha1r && xi(1) < lim_sup
+%     V1 = -kp*(xi(1) + delta/2) - kd*xi(3);
+%     if xi(2) > lim_inf/2 && xi(2) < alpha1r
+%         x_e = [alpha1r; pi; 0; 0];
+%         u_e = -1.7205;
+%         u = u_e - K*(xi-x_e);
+%         u_max = 8;   % Volt (esempio)
+%         u = max(min(u, u_max), -u_max);
+%         return
+%     elseif xi(2) > alpha1r && xi(2) < lim_sup
+%     x_e = [alpha1r; pi; 0; 0];
+%     u_e = -1.7205;
+%     u = (u_e - K*(xi-x_e));
+%     u_max = 8;   % Volt (esempio)
+%     u = max(min(u, u_max), -u_max);
+%     return
+%     end
+% else 
+%     % Virtual control (PD on first joint)
+%     V1 = -kp*(xi(1) - alpha1r) - kd*xi(3);
+% end
+% 
+% % if x(2) > lim_inf && x(2) < alpha1r
+% %         x_e = [alpha1r; pi; 0; 0];
+% %         u_e = -1.7205;
+% %         K = [-0.2165, -16.2534, -1.2892, -2.1864];
+% %         u = u_e - K*(xi-x_e);
+% %         u_max = 8;   % Volt (esempio)
+% %         u = max(min(u, u_max), -u_max);
+% %         return
+% %     end
+% 
+% % Torque
+% tau = m11_hat*V1 + Vmda1_hat + G1_hat;
+% 
+% % U = [p4*g*sin(alpha1r);
+% %      p5*g*sin(alpha1r)];
+% % tau_ref = U;
+% 
+% % Motor input
+% u = tau / k;
+% 
+% u_max = 8;   % Volt (esempio)
+% u = max(min(u, u_max), -u_max);
 
-%G(alpha)
-G = [p4*g*sin(xi(1)); p5*g*sin(xi(2))];
+% ---------- LQR REGION ----------
+alpha_tol  = 0.15;     % rad
+dalpha_tol = 0.4;
 
-m11_hat = m11 - (m12*m21/m22);
-Vmda1_hat = Vmda(1) - (m12*Vmda(2)/m22);
-G1_hat = G(1) - (m12*G(2)/m22);
+use_LQR = abs(alpha1-alpha1r) < alpha_tol && ...
+          abs(alpha2-pi)      < alpha_tol && ...
+          abs(dalpha1)        < dalpha_tol && ...
+          abs(dalpha2)        < dalpha_tol;
 
-tau = m11_hat*V1 + Vmda1_hat + G1_hat;
-u = tau/k;
+if use_LQR
+    % Equilibrium
+    x_e = [alpha1r; pi; 0; 0];
+    u_e = -1.7205;
 
+    % LQR gain
+    K = [-0.2165, -16.2534, -1.2892, -2.1864];
+
+    tau = u_e - K*(xi - x_e);
+
+else
+    % ---------- PFL + PD ----------
+    kp = 25;
+    kd = 15;
+
+    % Virtual control
+    V1 = -kp*(alpha1 - alpha1r) - kd*dalpha1;
+
+    % Torque
+    tau = m11_hat*V1 + Vmda1_hat + G1_hat;
+end
+
+%% ===== MOTOR INPUT =====
+u = tau / k;
+u = max(min(u, u_max), -u_max);
+
+end
